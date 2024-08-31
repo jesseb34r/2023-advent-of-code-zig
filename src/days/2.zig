97a2745ef915bedc8fbe5e1a46cfc1a2e69264ad
jesseb34r <jesseb34r@jesseaubin.me>
2024-08-30 17:18:31 -0700

    day2 part1 finished

--- src/days/2.zig
+++ src/days/2.zig
@@ -77,11 +77,11 @@ pub fn part1(
 ) !u64 {
     var sum: u64 = 0;
 
-    const bag = Set{.red = 12, .green = 13, .blue = 14};
+    const bag = Set{ .red = 12, .green = 13, .blue = 14 };
 
     while (input_lines.next()) |line| {
         const game = try Game.parseGame(allocator, line);
-        if (game.isPossible) {
+        if (game.isPossible(bag)) {
             sum += game.id;
         }
     }
@@ -97,7 +97,9 @@ pub fn part2(
 
     _ = allocator;
 
-    while (input_lines.next()) |line| {_ = line}
+    while (input_lines.next()) |line| {
+        _ = line;
+    }
 
     sum += 0;
 
@@ -152,11 +154,11 @@ test "impossibleGame" {
 
 test "part1" {
     const input =
-        \\ Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        \\ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-        \\ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-        \\ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-        \\ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        \\Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        \\Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        \\Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        \\Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        \\Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     ;
     const expected_result = 8;
     try utils.testPart(input, part1, expected_result);
