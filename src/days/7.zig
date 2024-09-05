b54b28cdd2e4b86a12004b569cf1eef2752c0ed2
jesseb34r <jesseb34r@jesseaubin.me>
2024-09-04 23:17:55 -0700

    day 7 finished

--- src/days/7.zig
+++ src/days/7.zig
@@ -15,7 +15,7 @@ const Card = enum(u8) {
     Four = 4,
     Three = 3,
     Two = 2,
-    // Joker = 1,
+    Joker = 1,
 
     const Self = @This();
 
@@ -38,24 +38,24 @@ const Card = enum(u8) {
         };
     }
 
-    // pub fn fromCharPartTwo(char: u8) !Self {
-    //     return switch (char) {
-    //         'A' => .Ace,
-    //         'K' => .King,
-    //         'Q' => .Queen,
-    //         'T' => .Ten,
-    //         '9' => .Nine,
-    //         '8' => .Eight,
-    //         '7' => .Seven,
-    //         '6' => .Six,
-    //         '5' => .Five,
-    //         '4' => .Four,
-    //         '3' => .Three,
-    //         '2' => .Two,
-    //         'J' => .Joker,
-    //         else => return error.InvalidCard,
-    //     };
-    // }
+    pub fn fromCharPartTwo(char: u8) !Self {
+        return switch (char) {
+            'A' => .Ace,
+            'K' => .King,
+            'Q' => .Queen,
+            'T' => .Ten,
+            '9' => .Nine,
+            '8' => .Eight,
+            '7' => .Seven,
+            '6' => .Six,
+            '5' => .Five,
+            '4' => .Four,
+            '3' => .Three,
+            '2' => .Two,
+            'J' => .Joker,
+            else => return error.InvalidCard,
+        };
+    }
 
     pub fn getRank(self: Self) u8 {
         return @intFromEnum(self);
@@ -75,65 +75,67 @@ const HandType = enum(u8) {
 
     pub fn parse(cards: [5]Card) !HandType {
         var counts = [_]u8{0} ** 15;
-        // var jokers: u8 = 0;
+        var jokers: u8 = 0;
         for (cards) |card| {
-            // if (card == .Joker) {
-            // jokers += 1;
-            // continue;
-            // }
+            if (card == .Joker) {
+                jokers += 1;
+                continue;
+            }
             counts[@intFromEnum(card)] += 1;
         }
 
-        // var hand_type: HandType = .HighCard;
-        var has_three = false;
-        var pairs: u8 = 0;
-        for (counts) |count| {
-            switch (count) {
-                5 => return .FiveOfAKind,
-                4 => return .FourOfAKind,
-                3 => has_three = true,
-                2 => pairs += 1,
-                else => {},
+        var hand_type: HandType = pre_joker: {
+            var has_three = false;
+            var pairs: u8 = 0;
+
+            for (counts) |count| {
+                switch (count) {
+                    5 => break :pre_joker .FiveOfAKind,
+                    4 => break :pre_joker .FourOfAKind,
+                    3 => has_three = true,
+                    2 => pairs += 1,
+                    else => {},
+                }
             }
+
+            if (has_three and pairs == 1) break :pre_joker .FullHouse;
+            if (has_three) break :pre_joker .ThreeOfAKind;
+            if (pairs == 2) break :pre_joker .TwoPair;
+            if (pairs == 1) break :pre_joker .OnePair;
+            break :pre_joker .HighCard;
+        };
+
+        switch (jokers) {
+            0 => {},
+            1 => {
+                switch (hand_type) {
+                    .FourOfAKind => hand_type = .FiveOfAKind,
+                    .ThreeOfAKind => hand_type = .FourOfAKind,
+                    .TwoPair => hand_type = .FullHouse,
+                    .OnePair => hand_type = .ThreeOfAKind,
+                    else => hand_type = .OnePair,
+                }
+            },
+            2 => {
+                switch (hand_type) {
+                    .ThreeOfAKind => hand_type = .FiveOfAKind,
+                    .OnePair => hand_type = .FourOfAKind,
+                    else => hand_type = .ThreeOfAKind,
+                }
+            },
+            3 => {
+                if (hand_type == .OnePair) {
+                    hand_type = .FiveOfAKind;
+                } else {
+                    hand_type = .FourOfAKind;
+                }
+            },
+            4 => hand_type = .FiveOfAKind,
+            5 => hand_type = .FiveOfAKind,
+            else => unreachable,
         }
 
-        if (has_three and pairs == 1) return .FullHouse;
-        if (has_three) return .ThreeOfAKind;
-        if (pairs == 2) return .TwoPair;
-        if (pairs == 1) return .OnePair;
-        return .HighCard;
-
-        // switch (jokers) {
-        //     0 => {},
-        //     1 => {
-        //         switch (hand_type) {
-        //             .FourOfAKind => hand_type = .FiveOfAKind,
-        //             .ThreeOfAKind => hand_type = .FourOfAKind,
-        //             .TwoPair => hand_type = .FullHouse,
-        //             .OnePair => hand_type = .ThreeOfAKind,
-        //             else => hand_type = .OnePair,
-        //         }
-        //     },
-        //     2 => {
-        //         switch (hand_type) {
-        //             .ThreeOfAKind => hand_type = .FiveOfAKind,
-        //             .OnePair => hand_type = .FourOfAKind,
-        //             else => hand_type = .ThreeOfAKind,
-        //         }
-        //     },
-        //     3 => {
-        //         if (hand_type == .OnePair) {
-        //             hand_type = .FiveOfAKind;
-        //         } else {
-        //             hand_type = .FourOfAKind;
-        //         }
-        //     },
-        //     4 => hand_type = .FiveOfAKind,
-        //     5 => hand_type = .FiveOfAKind,
-        //     else => unreachable,
-        // }
-
-        // return hand_type;
+        return hand_type;
     }
 
     pub fn getRank(self: Self) u8 {
@@ -166,23 +168,23 @@ const Hand = struct {
         return Self{ .cards = cards, .bid = bid, .hand_type = hand_type };
     }
 
-    // pub fn parsePartTwo(allocator: std.mem.Allocator, input: []const u8) !Self {
-    //     var hand_parts = std.mem.tokenizeSequence(u8, input, " ");
+    pub fn parsePartTwo(allocator: std.mem.Allocator, input: []const u8) !Self {
+        var hand_parts = std.mem.tokenizeSequence(u8, input, " ");
 
-    //     const cards_str = hand_parts.next().?;
-    //     var cards_arr = std.ArrayList(Card).init(allocator);
-    //     for (cards_str) |card_char| {
-    //         try cards_arr.append(try Card.fromCharPartTwo(card_char));
-    //     }
-    //     const cards: [5]Card = (try cards_arr.toOwnedSlice())[0..5].*;
+        const cards_str = hand_parts.next().?;
+        var cards_arr = std.ArrayList(Card).init(allocator);
+        for (cards_str) |card_char| {
+            try cards_arr.append(try Card.fromCharPartTwo(card_char));
+        }
+        const cards: [5]Card = (try cards_arr.toOwnedSlice())[0..5].*;
 
-    //     const bid_str = hand_parts.next().?;
-    //     const bid = try std.fmt.parseInt(u64, bid_str, 10);
+        const bid_str = hand_parts.next().?;
+        const bid = try std.fmt.parseInt(u64, bid_str, 10);
 
-    //     const hand_type = try HandType.parse(cards);
+        const hand_type = try HandType.parse(cards);
 
-    //     return Self{ .cards = cards, .bid = bid, .hand_type = hand_type };
-    // }
+        return Self{ .cards = cards, .bid = bid, .hand_type = hand_type };
+    }
 
     pub fn compareHands(context: void, a: Hand, b: Hand) bool {
         _ = context;
@@ -224,8 +226,8 @@ pub fn part2(
     var hand_set = std.ArrayList(Hand).init(allocator);
 
     while (input_lines.next()) |line| {
-        // try hand_set.append(try Hand.parsePartTwo(allocator, line));
-        try hand_set.append(try Hand.parse(allocator, line));
+        try hand_set.append(try Hand.parsePartTwo(allocator, line));
+        // try hand_set.append(try Hand.parse(allocator, line));
     }
 
     std.sort.insertion(Hand, hand_set.items, {}, Hand.compareHands);
