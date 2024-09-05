a57dab2d278cf76db8f770201736c82482eada22
jesseb34r <jesseb34r@jesseaubin.me>
2024-09-05 01:45:27 -0700

    day 9 part 1 done

--- src/days/9.zig
+++ src/days/9.zig
@@ -5,7 +5,7 @@ pub fn part1(
     allocator: std.mem.Allocator,
     input_lines: *std.mem.TokenIterator(u8, .sequence),
 ) !u64 {
-    // var sum: u64 = 0;
+    var sum: i64 = 0;
 
     while (input_lines.next()) |line| {
         var value_iter = std.mem.tokenizeSequence(u8, line, " ");
@@ -19,34 +19,43 @@ pub fn part1(
         value_iter.reset();
 
         var current_array: usize = 0;
-        var all_zeros = false;
-        while (!all_zeros) : (current_array += 1) {
+        while (true) : (current_array += 1) {
             var i: usize = 0;
             try value_arrays.append(std.ArrayList(i64).init(allocator));
             while (i < value_arrays.items[current_array].items.len - 1) : (i += 1) {
                 try value_arrays.items[current_array + 1].append(value_arrays.items[current_array].items[i + 1] - value_arrays.items[current_array].items[i]);
             }
 
-            all_zeros = for (value_arrays.items[current_array + 1].items) |value| {
+            const all_zeros = for (value_arrays.items[current_array + 1].items) |value| {
                 if (value != 0) break false;
             } else true;
+
+            if (all_zeros) {
+                try value_arrays.items[current_array + 1].append(0);
+                current_array += 1;
+                while (current_array > 0) : (current_array -= 1) {
+                    try value_arrays.items[current_array - 1].append(value_arrays.items[current_array - 1].getLast() + value_arrays.items[current_array].getLast());
+                }
+                sum += value_arrays.items[0].getLast();
+                break;
+            }
         }
 
-        for (value_arrays.items, 0..) |array, index| {
-            // Print leading spaces for triangular shape
-            for (0..index) |_| {
-                std.debug.print("  ", .{});
-            }
-            // Print the array contents
-            for (array.items) |value| {
-                std.debug.print("{d:4} ", .{@abs(value)});
-            }
-            std.debug.print("\n", .{});
-        }
-        std.debug.print("\n", .{});
+        // for (value_arrays.items, 0..) |array, index| {
+        //     // Print leading spaces for triangular shape
+        //     for (0..index) |_| {
+        //         std.debug.print("  ", .{});
+        //     }
+        //     // Print the array contents
+        //     for (array.items) |value| {
+        //         std.debug.print("{d:4} ", .{@abs(value)});
+        //     }
+        //     std.debug.print("\n", .{});
+        // }
+        // std.debug.print("\n", .{});
     }
 
-    return 0;
+    return @abs(sum);
 }
 
 pub fn part2(
@@ -70,7 +79,7 @@ test "part1" {
         \\1 3 6 10 15 21
         \\10 13 16 21 30 45
     ;
-    const expected_result = 0;
+    const expected_result = 114;
     try utils.testPart(input, part1, expected_result);
 }
 
