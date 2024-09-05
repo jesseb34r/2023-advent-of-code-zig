1c08955a75aa4e7553923002db71a1eb1e778446
jesseb34r <jesseb34r@jesseaubin.me>
2024-09-05 01:31:21 -0700

    day 9 parse input into triangular array of arrays

--- src/days/9.zig
+++ src/days/9.zig
@@ -1,19 +1,58 @@
 const std = @import("std");
 const utils = @import("utils");
 
-pub fn part1(allocator: std.mem.Allocator, input_lines: *std.mem.TokenIterator(u8, .sequence),) !u64 {
-    _ = allocator;
-    var sum: u64 = 0;
+pub fn part1(
+    allocator: std.mem.Allocator,
+    input_lines: *std.mem.TokenIterator(u8, .sequence),
+) !u64 {
+    // var sum: u64 = 0;
 
     while (input_lines.next()) |line| {
-        _ = line;
-    }
-    sum += 0;
+        var value_iter = std.mem.tokenizeSequence(u8, line, " ");
 
-    return sum;
+        var value_arrays = std.ArrayList(std.ArrayList(i64)).init(allocator);
+
+        try value_arrays.append(std.ArrayList(i64).init(allocator));
+        while (value_iter.next()) |value| {
+            try value_arrays.items[0].append(try std.fmt.parseInt(i64, value, 10));
+        }
+        value_iter.reset();
+
+        var current_array: usize = 0;
+        var all_zeros = false;
+        while (!all_zeros) : (current_array += 1) {
+            var i: usize = 0;
+            try value_arrays.append(std.ArrayList(i64).init(allocator));
+            while (i < value_arrays.items[current_array].items.len - 1) : (i += 1) {
+                try value_arrays.items[current_array + 1].append(value_arrays.items[current_array].items[i + 1] - value_arrays.items[current_array].items[i]);
+            }
+
+            all_zeros = for (value_arrays.items[current_array + 1].items) |value| {
+                if (value != 0) break false;
+            } else true;
+        }
+
+        for (value_arrays.items, 0..) |array, index| {
+            // Print leading spaces for triangular shape
+            for (0..index) |_| {
+                std.debug.print("  ", .{});
+            }
+            // Print the array contents
+            for (array.items) |value| {
+                std.debug.print("{d:4} ", .{@abs(value)});
+            }
+            std.debug.print("\n", .{});
+        }
+        std.debug.print("\n", .{});
+    }
+
+    return 0;
 }
 
-pub fn part2(allocator: std.mem.Allocator, input_lines: *std.mem.TokenIterator(u8, .sequence),) !u64 {
+pub fn part2(
+    allocator: std.mem.Allocator,
+    input_lines: *std.mem.TokenIterator(u8, .sequence),
+) !u64 {
     _ = allocator;
     var sum: u64 = 0;
 
@@ -27,10 +66,9 @@ pub fn part2(allocator: std.mem.Allocator, input_lines: *std.mem.TokenIterator(u
 
 test "part1" {
     const input =
-        \\
-        \\
-        \\
-        \\
+        \\0 3 6 9 12 15
+        \\1 3 6 10 15 21
+        \\10 13 16 21 30 45
     ;
     const expected_result = 0;
     try utils.testPart(input, part1, expected_result);
