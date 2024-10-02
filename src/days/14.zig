d603af35776086ef21900ba37dc3044a26e5dddc
jesseb34r <jesseb34r@jesseaubin.me>
2024-10-01 23:26:37 -0700

    use struct from part 2 for part 1

--- src/days/14.zig
+++ src/days/14.zig
@@ -10,73 +10,9 @@ pub fn part1(
     allocator: std.mem.Allocator,
     input: []u8,
 ) !u64 {
-    var input_lines = std.mem.tokenizeSequence(u8, input, "\n");
-
-    var cols_arr = std.ArrayList([]u8).init(allocator);
-    for (0..input_lines.peek().?.len) |col_index| {
-        var col_builder = std.ArrayList(u8).init(allocator);
-
-        while (input_lines.next()) |line| {
-            try col_builder.append(line[col_index]);
-        }
-        try cols_arr.append(try col_builder.toOwnedSlice());
-
-        input_lines.reset();
-    }
-
-    // std.debug.print("Columns before tilting north:\n", .{});
-    // for (cols_arr.items) |col| {
-    //     std.debug.print("{s}\n", .{col});
-    // }
-
-    // tilt board north, moving all 'O's as far north as possible
-    columns: for (0..cols_arr.items.len) |col_index| {
-        var farthest_open_index = firstIndexOf(cols_arr.items[col_index], '.');
-
-        positions: for (0..cols_arr.items[col_index].len) |pos_index| {
-            // std.debug.print(
-            //     "Pos index: {d}, Farthest open index: {?d}\n",
-            //     .{ pos_index, farthest_open_index },
-            // );
-            if (farthest_open_index == null) continue :columns;
-            if (pos_index <= farthest_open_index.?) continue :positions;
-
-            switch (cols_arr.items[col_index][pos_index]) {
-                'O' => {
-                    cols_arr.items[col_index][farthest_open_index.?] = 'O';
-                    cols_arr.items[col_index][pos_index] = '.';
-                    const next_open_index = firstIndexOf(
-                        cols_arr.items[col_index][(farthest_open_index.? + 1)..],
-                        '.',
-                    );
-                    farthest_open_index = if (next_open_index == null) null else next_open_index.? + farthest_open_index.? + 1;
-                },
-                '#' => {
-                    const next_open_index = firstIndexOf(
-                        cols_arr.items[col_index][(pos_index + 1)..],
-                        '.',
-                    );
-                    farthest_open_index = if (next_open_index == null) null else next_open_index.? + pos_index + 1;
-                },
-                '.' => continue :positions,
-                else => return error.InvalidCharacter,
-            }
-        }
-    }
-
-    // std.debug.print("Columns after tilting north:\n", .{});
-    // for (cols_arr.items) |col| {
-    //     std.debug.print("{s}\n", .{col});
-    // }
-
-    var sum: u64 = 0;
-    for (cols_arr.items) |col| {
-        for (col, 0..) |pos, i| {
-            if (pos == 'O') sum += col.len - i;
-        }
-    }
-
-    return sum;
+    var platform = try Platform.init(allocator, input);
+    platform.shiftNorth();
+    return platform.calcLoad();
 }
 
 const Platform = struct {
