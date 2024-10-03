dcb6891fb9580358f68c44998e87ec0fd55e20c1
jesseb34r <jesseb34r@jesseaubin.me>
2024-10-03 11:27:26 -0700

    begin work on day 17

--- src/days/17.zig
+++ src/days/17.zig
@@ -1,20 +1,38 @@
 const std = @import("std");
 const utils = @import("utils");
 
+const Map = struct {
+    grid: [][]u8,
+    allocator: std.mem.Allocator,
+
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator, input: []u8) !Self {
+        var grid_builder = std.ArrayList([]u8).init(allocator);
+        var input_lines = std.mem.tokenizeSequence(u8, input, "\n");
+        while (input_lines.next()) |line| {
+            var row = try allocator.alloc(u8, line.len);
+            for (line, 0..) |val, i| {
+                row[i] = val;
+            }
+            try grid_builder.append(row);
+        }
+
+        return Self{ .grid = try grid_builder.toOwnedSlice(), .allocator = allocator };
+    }
+};
+
 pub fn part1(
     allocator: std.mem.Allocator,
     input: []u8,
 ) !u64 {
-    var input_lines = std.mem.tokenizeSequence(u8, input, "\n");
-    _ = allocator;
-    var sum: u64 = 0;
+    const map = try Map.init(allocator, input);
 
-    while (input_lines.next()) |line| {
-        _ = line;
+    for (map.grid) |row| {
+        std.debug.print("{s}\n", .{row});
     }
-    sum += 0;
 
-    return sum;
+    return 0;
 }
 
 pub fn part2(
@@ -34,23 +52,57 @@ pub fn part2(
 }
 
 test "part1" {
+    var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_allocator.deinit();
+    const arena = arena_allocator.allocator();
+
     const input =
-        \\
-        \\
-        \\
-        \\
+        \\2413432311323
+        \\3215453535623
+        \\3255245654254
+        \\3446585845452
+        \\4546657867536
+        \\1438598798454
+        \\4457876987766
+        \\3637877979653
+        \\4654967986887
+        \\4564679986453
+        \\1224686865563
+        \\2546548887735
+        \\4322674655533
     ;
-    const expected_result = 0;
-    try utils.testPart(input, part1, expected_result);
+    const mutable = try arena.alloc(u8, input.len);
+    std.mem.copyForwards(u8, mutable, input);
+
+    const expected_result = 102;
+    const result = try part1(arena, mutable);
+    try std.testing.expectEqual(expected_result, result);
 }
 
 test "part2" {
+    var arena_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena_allocator.deinit();
+    const arena = arena_allocator.allocator();
+
     const input =
-        \\
-        \\
-        \\
-        \\
+        \\2413432311323
+        \\3215453535623
+        \\3255245654254
+        \\3446585845452
+        \\4546657867536
+        \\1438598798454
+        \\4457876987766
+        \\3637877979653
+        \\4654967986887
+        \\4564679986453
+        \\1224686865563
+        \\2546548887735
+        \\4322674655533
     ;
+    const mutable = try arena.alloc(u8, input.len);
+    std.mem.copyForwards(u8, mutable, input);
+
     const expected_result = 0;
-    try utils.testPart(input, part2, expected_result);
+    const result = try part2(arena, mutable);
+    try std.testing.expectEqual(expected_result, result);
 }
